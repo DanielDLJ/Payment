0b6c0dc68d42aac2a53eb1caecd52c7f07e869ad
Daniel Leme Junior <daniel.dlj@hotmail.com>
2023-04-08 22:25:07 -0300

    PlanModule

---
+++ test/plan/plan.controller.spec.ts
@@ -0,0 +1,18 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { PlanController } from '../../src/plan/plan.controller';
+
+describe('PlanController', () => {
+  let controller: PlanController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [PlanController],
+    }).compile();
+
+    controller = module.get<PlanController>(PlanController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
