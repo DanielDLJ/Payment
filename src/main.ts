bd12f02ab5a44b0d2f8ee82c5ee3b72915ae19c8
Daniel Leme Junior <daniel.dlj@hotmail.com>
2023-04-15 18:33:13 -0300

    configSwagger

--- src/main.ts
+++ src/main.ts
@@ -2,22 +2,41 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { Logger, LoggerErrorInterceptor } from "nestjs-pino";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import * as dotenv from "dotenv";
+import { INestApplication } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+
+dotenv.config();
+
+function configSwagger(app: INestApplication) {
+  const configService = app.get(ConfigService);
+
+  const documentBuilder = new DocumentBuilder()
+    .setTitle(configService.get<string>("swagger.title"))
+    .setDescription(configService.get<string>("swagger.description"))
+    .setContact(
+      configService.get<string>("swagger.contact.name"),
+      "",
+      configService.get<string>("swagger.contact.email"),
+    )
+    .setVersion(configService.get<string>("swagger.version"))
+    .build();
+
+  const document = SwaggerModule.createDocument(app, documentBuilder);
+  SwaggerModule.setup("swagger", app, document);
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
+  const configService = app.get(ConfigService);
 
   app.useLogger(app.get(Logger));
   app.useGlobalInterceptors(new LoggerErrorInterceptor());
   app.flushLogs();
 
-  const config = new DocumentBuilder()
-    .setTitle("Payment")
-    .setDescription("Payment management service")
-    .setContact("Daniel Leme Junior", "", "daniel.dlj@hotmail.com")
-    .setVersion("1.0")
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup("swagger", app, document);
+  if (configService.get<boolean>("swagger.enable")) {
+    configSwagger(app);
+  }
 
   await app.listen(3001);
 }
