fbfb7a7111f7265b4c638a7914ba1ea802ef8614
Daniel Leme Junior <daniel.dlj@hotmail.com>
2023-04-15 21:30:03 -0300

    feat: added ValidationPipe

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from "./app.module";
 import { Logger, LoggerErrorInterceptor } from "nestjs-pino";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import * as dotenv from "dotenv";
-import { INestApplication } from "@nestjs/common";
+import { INestApplication, ValidationPipe } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 
 dotenv.config();
@@ -30,6 +30,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
   const configService = app.get(ConfigService);
 
+  app.useGlobalPipes(
+    new ValidationPipe({
+      forbidUnknownValues: true,
+      forbidNonWhitelisted: true,
+      whitelist: true,
+    }),
+  );
   app.useLogger(app.get(Logger));
   app.useGlobalInterceptors(new LoggerErrorInterceptor());
   app.flushLogs();
